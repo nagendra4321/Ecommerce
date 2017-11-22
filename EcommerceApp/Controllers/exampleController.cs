1c3744a6afe7b2424474b4c8b19ee9597b736b11
nagendrababuus <nagendrababuus@gmail.com>
2017-11-21 19:55:31 -0800

    examplechanges

---
+++ EcommerceApp/Controllers/exampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceApp.Controllers
+{
+    public class exampleController : Controller
+    {
+        // GET: example
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
