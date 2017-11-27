3760481d02556dce7518466112d57c7a26ad869d
nagendrababuus <nagendrababuus@gmail.com>
2017-11-26 16:38:25 -0800

    after adding ItemController

---
+++ EcommerceApp/Controllers/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcommerceApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetItem( int id)
+        {
+            return View();
+        }
+    }
+}
